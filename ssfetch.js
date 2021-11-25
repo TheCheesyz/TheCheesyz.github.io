9e316c3b6db6b665d296f5afe74e5b7269c32c59
Zephram Carroll <55356060+ZRPCarroll@users.noreply.github.com>
2021-11-25 13:16:10 -0500

    Added Tools

--- ssfetch.js
+++ ssfetch.js
@@ -1,9 +1,29 @@
+var peopleArray = [];
+
 function whoIsMyGuy() {
-	fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => {
-			return response.json();
-		})
-		.then(users => {
-			document.getElementById("myGuy").innerHTML=users[0].name;
-		});
+	var code = document.getElementById('codeInput').value;
+	document.getElementById("myGuy").innerHTML = code;
+
+}
+
+function generateSecrets() {
+	shuffleArray(peopleArray);
+	console.log(peopleArray);
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function addToList() {
+	var person = {
+		name : document.getElementById('name').value,
+		email : document.getElementById('email').value
+	}
+	peopleArray.push(person);
 }
