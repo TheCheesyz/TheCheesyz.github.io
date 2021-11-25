87afed9298a70dd1d9f867162180a2eb9081ff88
Zephram Carroll <55356060+ZRPCarroll@users.noreply.github.com>
2021-11-25 13:31:45 -0500

    Complete tools for testing

--- ssfetch.js
+++ ssfetch.js
@@ -1,13 +1,28 @@
+const CryptoJS = require("crypto-js");
+const encrypt = (text) => {
+  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(text));
+};
+const decrypt = (data) => {
+  return CryptoJS.enc.Base64.parse(data).toString(CryptoJS.enc.Utf8);
+};
+
+
 var peopleArray = [];
 
 function whoIsMyGuy() {
 	var code = document.getElementById('codeInput').value;
-	document.getElementById("myGuy").innerHTML = code;
-
+	document.getElementById("myGuy").innerHTML = decrypt(code);
 }
 
 function generateSecrets() {
 	shuffleArray(peopleArray);
+	for(var i = 0; i < array.length; i++){
+		if(i == array.length - 1){
+			peopleArray[array.length - 1].name = encrypt(peopleArray[0].name);
+		} else{
+			peopleArray[i].name = encrypt(peopleArray[i+1].name);
+		}
+	}
 	console.log(peopleArray);
 }
 
@@ -26,4 +41,5 @@ function addToList() {
 		email : document.getElementById('email').value
 	}
 	peopleArray.push(person);
+	console.log(person);
 }
